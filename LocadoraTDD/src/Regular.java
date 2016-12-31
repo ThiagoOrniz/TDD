c7bfd7b276badff186348ab812cf52391198efc4
Thiago Orniz Martin <thiago.orniz@gmail.com>
2016-12-31 00:51:13 -0200

    project locadora refactored added

---
+++ LocadoraTDD/src/Regular.java
@@ -0,0 +1,18 @@
+
+public class Regular extends Movie {
+
+	public Regular(String title) {
+		super(title);
+
+	}
+
+	@Override
+	public double getAmount(int daysRented) {
+		double thisAmount = 2;
+		if (daysRented > 2)
+				thisAmount += (daysRented - 2) * 1.5;
+		
+		return thisAmount;
+	}
+
+}
